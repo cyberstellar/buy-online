3980a20f70174dc2d599a02963cc68672924f885
cyberstellar <60780438+cyberstellar@users.noreply.github.com>
2023-09-28 17:44:18 +0300

    Уменьшено количество перерисовок кнопок "Добавить в корзину" и "Добавить в избранное".
    Раньше при клике на кнопку происходила перерисовка всех кнопок подобного типа, т.к. состояние каждой отдельной кнопки напрямую зависело от глобального.
    
    Теперь глобальное состояние используется только для первичной инициализации.

--- src/components/buttons/add-to-cart-button/add-to-cart-button.js
+++ src/components/buttons/add-to-cart-button/add-to-cart-button.js
@@ -1,25 +1,33 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { addToCart } from '../../../services/dummy-api/cartSlice'
+import { store } from '../../../services/dummy-api'
 import Button from '../button'
 import './add-to-cart-button.scss'
 
-const AddToCartButton = ({ id, quantity = 1 }) => {
-  const dispatch = useDispatch()
-  const order = useSelector(state => state.cart.products)
-  const [isActive, setIsActive] = useState(false)
+const AddToCartButton = ({ id }) => {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const [isActive, setIsActive] = useState(false)
 
   useEffect(() => {
-    order.find(x => x.id === id)
-      ? setIsActive(true)
-      : setIsActive(false)
-  }, [id, order])
+    store.getState().cart.products.find(x => x.id === id)
+    && setIsActive(true)
+  }, [])
+
+  const handleClick = () => {
+    if (isActive) {
+      navigate('/cart')
+    } else {
+      dispatch(addToCart({ id }))
+      setIsActive(true)
+    }
+  }
 
   return (
     <Button className="cart-button"
-            onClick={() => isActive ? navigate('/cart') : dispatch(addToCart({ id }))}
+            onClick={handleClick}
             iconName="shopping_cart"
             text={isActive ? 'В корзине' : 'В корзину'}
             isActive={isActive}
--- src/components/buttons/add-to-wishlist-button/add-to-wishlist-button.js
+++ src/components/buttons/add-to-wishlist-button/add-to-wishlist-button.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { addToFavorites } from '../../../services/dummy-api/cartSlice'
+import { store } from '../../../services/dummy-api'
 import Button from '../button'
 import './add-to-wishlist-button.scss'
 
 const AddToWishlistButton = ({ id }) => {
   const dispatch = useDispatch()
-  const favorites = useSelector(state => state.cart.favorites)
+  const [isActive, setIsActive] = useState(false)
+
+  useEffect(() => {
+    store.getState().cart.favorites.includes(id)
+    && setIsActive(true)
+  }, [])
+
+  const handleClick = () => {
+    dispatch(addToFavorites({ id }))
+    setIsActive(!isActive)
+  }
 
   return (
     <Button className="rounded square wishlist-button"
-            onClick={() => dispatch(addToFavorites({ id }))}
+            onClick={handleClick}
             iconName="favorite"
-            isActive={favorites.includes(id)}
+            isActive={isActive}
     />
   )
 }
